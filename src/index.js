ffd57eb7764df4893fe59b4b620d365bb99888e1
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-02-07 19:51:17 +0700

    debug

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-import express from 'express';
+
+const express = require('express');
 const app = express();
-import path from 'path';
+const path = require('path');
 const port = 3000;
 
-import fs from 'fs';
-import { PythonShell } from 'python-shell';
-import XLSX from 'xlsx';
-
-
-import { execSync, exec } from 'child_process';
-import { promisify } from 'util';
-import { appendFile } from 'fs';
-import which from 'which';
-import https from 'https';
-import opn from 'opn';
+const fs = require('fs');
 
 
 // Set the PYTHONHOME and PATH environment variables
@@ -21,17 +12,14 @@ import opn from 'opn';
   // process.env.PYTHONHOME = path.join(__dirname, '../src/pythons');
   // process.env.PATH = `${process.env.PYTHONHOME};${process.env.PATH}`;
 
-// Use import.meta.url to get the module URL and then extract the directory
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-
 process.env.PYTHONHOME = path.join(__dirname, 'pythons');
+process.env.PATH = `${process.env.PYTHONHOME};${process.env.PATH}`;
 
 app.use(express.static(__dirname + '/'));
 
 // เส้นทางหลักของแอปพลิเคชัน
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, 'src/index.html');
+  const indexPath = path.join(__dirname, '/src/index.html');
   res.sendFile(indexPath);
   console.log(`Current path: ${indexPath}`);
 });
