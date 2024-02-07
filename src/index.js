f00dfd93e306e469c11ede22f2f5b241fbed2561
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-02-07 15:28:51 +0700

    debug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-// Import required modules
-import express from 'express';
-import path from 'path';
-import fs from 'fs';
 
-// Create an express app
+const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+const fs = require('fs');
 
 
 // Set the PYTHONHOME and PATH environment variables
@@ -15,19 +12,14 @@ const port = 3000;
   // process.env.PYTHONHOME = path.join(__dirname, '../src/pythons');
   // process.env.PATH = `${process.env.PYTHONHOME};${process.env.PATH}`;
 
-// Get the directory name of the current module file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Set the PYTHONHOME environment variable
 process.env.PYTHONHOME = path.join(__dirname, 'pythons');
-
+process.env.PATH = `${process.env.PYTHONHOME};${process.env.PATH}`;
 
 app.use(express.static(__dirname + '/'));
 
 // เส้นทางหลักของแอปพลิเคชัน
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, '/src/index.html');
+  const indexPath = path.join(__dirname, 'src/index.html');
   res.sendFile(indexPath);
   console.log(`Current path: ${indexPath}`);
 });
