6a627677eb744b779c9aeecce08c1b16255b3c46
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2024-02-08 22:33:53 +0700

    denug

--- src/page/tools/product_dimension_generate.js
+++ src/page/tools/product_dimension_generate.js
@@ -19,7 +19,7 @@
 
 
 
-{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> */}
+// {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> */}
 
     //get dropdown list
     function get_option_list_dimension() {
